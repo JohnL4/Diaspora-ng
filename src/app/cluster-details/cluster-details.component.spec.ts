7ee07d868f695315e049dab80af1dd85547e3c97
John Lusk <john-github@how-hard-can-it-be.com>
2016-08-09 15:01:31 -0400

    Issue #7 -- fix unit test.

--- src/app/cluster-details/cluster-details.component.spec.ts
+++ src/app/cluster-details/cluster-details.component.spec.ts
@@ -4,10 +4,11 @@ import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { addProviders, async, inject } from '@angular/core/testing';
 import { ClusterDetailsComponent } from './cluster-details.component';
+import { Cluster } from '../cluster';
 
 describe('Component: ClusterDetails', () => {
   it('should create an instance', () => {
-    let component = new ClusterDetailsComponent();
+     let component = new ClusterDetailsComponent( new Cluster());
     expect(component).toBeTruthy();
   });
 });
