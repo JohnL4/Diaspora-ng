8c4f8d32e60931c9bc6599c90ae0f6fc0992b854
John Lusk <john-github@how-hard-can-it-be.com>
2016-11-12 19:51:04 -0500

    Added routes, raw Bootstrap, raw jQuery. Still need to use npm & webpack to manage them.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ClusterDetailsComponent } from './cluster-details/cluster-details.component';
@@ -11,20 +12,38 @@ import { TabsComponent } from './tabs/tabs.component';
 import { XmlComponent } from './xml/xml.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ClusterDetailsComponent,
-    DotComponent,
-    GeneratorParamsComponent,
-    TabsComponent,
-    XmlComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ClusterDetailsComponent,
+      DotComponent,
+      GeneratorParamsComponent,
+      TabsComponent,
+      XmlComponent
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule,
+      RouterModule.forRoot([
+         {
+            path: 'params',
+            component: GeneratorParamsComponent
+         },
+         {
+            path: 'details',
+            component: ClusterDetailsComponent
+         },
+         {
+            path: 'xml',
+            component: XmlComponent
+         },
+         {
+            path: 'dot',
+            component: DotComponent
+         }
+      ])
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
