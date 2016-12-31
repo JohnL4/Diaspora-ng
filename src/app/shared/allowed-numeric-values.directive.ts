f78b448082ed851bda05d3dcca16e8e7b82be908
John Lusk <john-github@how-hard-can-it-be.com>
2016-12-31 13:40:23 -0500

    Improved validator now allows negative numbers and exclusive ranges.

--- src/app/shared/allowed-numeric-values.directive.ts
+++ src/app/shared/allowed-numeric-values.directive.ts
@@ -10,37 +10,77 @@ const SELECTOR: string = 'allowedNumericValues'; // TODO: figure out if this is 
 
 class Range
 {
-   constructor ( public Low: number, public High: number) {}
+   /**
+    * "exclusive" booleans <==> corresponding endpoint is exclusive. Default is obviously non-exclusive (i.e., inclusive).
+    */
+   constructor ( public Low: number, public High: number, public LowExclusive: boolean, public HighExclusive: boolean)
+   {
+      if (Low > High) throw new Error( `Illegal range, Low (${Low}) > High (${High})`);
+   }
 }
 
+/**
+ * Validate numbers given an "allowed values specification" in the form "i;j;[k,m];(n,p);[,q);[r,]", which means the
+ * following values are allowed:
+ *
+ * <ul>
+ *    <li> i
+ *    <li> j
+ *    <li> All values in the range [k,m] (inclusive) -- k must be less than or equal to m
+ *    <li> All values in the range (n,p) (exclusive) -- same restriction on n, p as above
+ *    <li> All values less than q
+ *    <li> All values greater than or equal to r
+ * </ul>
+ *
+ * Note the inclusivity and exclusivity on endpoints can be mixed, as in [0.0,100.0).  All ranges must have a comma and
+ * at least one endpoint.
+ */
 export function allowedNumericValuesValidator( anAllowedValuesSpec: string): ValidatorFn
 {
    console.log( anv_me + ": running allowedNumericValuesValidator()");
    let errors: string[] = [];   // Errors from parsing allowed values specification
    let ranges : Range[] = [];   // Allowed ranges, used in validation.
-   let rangeSpecs = anAllowedValuesSpec.split( /\s*,\s*/);
+   let rangeSpecs = anAllowedValuesSpec.split( /\s*;\s*/);
    for (let r of rangeSpecs)
    {
-      let ends : string[] = r.split( /\s*-\s*/); // TODO: allow negative numbers in range. How?
-      if (ends.length == 1)
+      let ends : string[] = r.split( /\s*,\s*/);
+      try
       {
-         let end : number = Number(ends[0]);
-         if (isNaN( end))
-            errors.push( r + " is NaN");
+         if (ends.length == 1)
+         {
+            // Plain number, not a range
+            let end : number = Number(ends[0]);
+            if (isNaN( end))
+               errors.push( "NaN: " + r);
+            else
+               ranges.push( new Range( end, end, false, false));
+         }
+         else if (ends.length == 2)
+         {
+            // Leading, trailing whitespace should have already been stripped by split() calls.
+            // range opener plus low endpoint, modulo whitespace.
+            // TODO: unit-test these regexps.
+            let lowMatches = ends[0].match( /^([[(])(.*)/); // $2 may be empty (whitespace only)
+            let highMatches = ends[1].match( /([^\])]*)([\])])/); // $1 may be empty (whitespace only)
+            if (! lowMatches || ! highMatches)
+               errors.push( "Bad range syntax: " + r);
+            else
+            {
+               let low = toNum( lowMatches[2], -Infinity);
+               let high = toNum( highMatches[1], +Infinity);
+               let lowExcl: boolean = lowMatches[1].match( /\(/) ? true : false;
+               let highExcl: boolean = highMatches[2].match( /\)/) ? true : false;
+               ranges.push( new Range( low, high, lowExcl, highExcl));
+            }
+         }
          else
-            ranges.push( new Range( end, end));
+            // Too many commas or something.
+            errors.push( "Bad range syntax: " + r);
       }
-      else if (ends.length == 2)
+      catch (exc)
       {
-         let low:number = Number(ends[0]);
-         let high:number = Number(ends[1]);
-         if (isNaN( low) || isNaN( high))
-            errors.push( r + " has NaN");
-         else
-            ranges.push( new Range( low, high));
+         errors.push( exc.toString());
       }
-      else
-         errors.push( r + " has bad syntax");
    }
    if (errors.length > 0)
       throw new Error( errors.join( "; "));
@@ -50,11 +90,12 @@ export function allowedNumericValuesValidator( anAllowedValuesSpec: string): Val
       const numberToBeValidated = control.value;
       const num = Number( numberToBeValidated);
       if (isNaN( num))
-         return {SELECTOR: {numberToBeValidated}};
+         return {[SELECTOR]: {numberToBeValidated}}; // [SELECTOR]: ES6 computed property name
       let isGood: boolean = false;
       for (let r of ranges)
       {
-         if (r.Low <= num && num <= r.High)
+         if (((r.LowExclusive && r.Low < num) || (! r.LowExclusive && r.Low <= num))
+             && ((r.HighExclusive && num < r.High) || (! r.HighExclusive && num <= r.High)))
          {
             isGood = true;
             break;
@@ -62,6 +103,23 @@ export function allowedNumericValuesValidator( anAllowedValuesSpec: string): Val
       }
       return isGood ? null : {[SELECTOR]: {numberToBeValidated}}; // [SELECTOR]: ES6 computed property name
    };
+}
+
+/**
+ * Convert string to number, with default for whitespace, throwing exception on NaN.
+ */
+function toNum( aString: string, aDefault: number): number
+{
+   let num;
+   if (aString.match( /^\s*$/))
+      num = aDefault;
+   else
+   {
+      num = Number( aString);
+      if (isNaN( num))
+         throw new Error( `isNan: ${aString}`);
+   }
+   return num;
 }
 
 @Directive({
