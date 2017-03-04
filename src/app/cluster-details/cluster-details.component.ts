8931e2c3b3051b05078692c77ad7ae0d350b5556
John Lusk <john-github@how-hard-can-it-be.com>
2017-03-04 13:17:04 -0500

    Showing graph of cluster, laid out with COSE algorithm in Cytoscape.

--- src/app/cluster-details/cluster-details.component.ts
+++ src/app/cluster-details/cluster-details.component.ts
@@ -34,28 +34,11 @@ export class ClusterDetailsComponent implements OnInit {
       // let cose_bilkent = require( 'cytoscape-cose-bilkent');
       // cose_bilkent( cytoscape);
 
+      let graphElements: Array<any> = this.elementsGraph( this._cluster);
       let cyDiv = document.getElementById( 'cytoscapeDiv');
       let cy = cytoscape({
          container: cyDiv,
-         elements: [
-            { data: {id: 'Alpha'}},
-            { data: {id: 'Bravo'}},
-            { data: {id: 'Charlie'}},
-            { data: {id: 'Delta'}},
-            { data: {id: 'Echo'}},
-
-            { data: {id: 'ab', source: 'Alpha', target: 'Bravo'}},
-            { data: {id: 'bc', source: 'Bravo', target: 'Charlie'}},
-            { data: {id: 'ac', source: 'Alpha', target: 'Charlie'}},
-            { data: {id: 'cd', source: 'Charlie', target: 'Delta'}},
-            { data: {id: 'da', source: 'Delta', target: 'Alpha'}},
-            { data: {id: 'db', source: 'Delta', target: 'Bravo'}},
-
-            // { data: {id: 'ae', source: 'Alpha', target: 'Echo'}},
-            // { data: {id: 'be', source: 'Bravo', target: 'Echo'}},
-            { data: {id: 'ce', source: 'Charlie', target: 'Echo'}},
-            { data: {id: 'de', source: 'Delta', target: 'Echo'}},
-         ],
+         elements: graphElements,
          style: [
             {
                selector: 'node',
@@ -67,7 +50,7 @@ export class ClusterDetailsComponent implements OnInit {
                selector: 'edge',
                style: {
                   'line-color': 'hsl(240, 100%, 80%)',
-                  'curve-style': 'bezier',
+                  'curve-style': 'bezier', // Just in case we have two slipstreams between the same two systems.
                   'control-point-step-size': 20,
                   'source-label': '+',
                   'target-label': '-',
@@ -82,4 +65,42 @@ export class ClusterDetailsComponent implements OnInit {
       console.log( `loaded cytoscape; cyDiv = ${cyDiv}; cy = ${cy} `);
   }
 
+   /**
+    * Returns an array of Cytoscape graph elements (nodes and edges).
+    */
+   private elementsGraph( aCluster: Cluster): Array<any>
+   {
+      let retval: Array<any> = new Array<any>();
+//         = [
+//            { data: {id: 'Alpha'}},
+//            { data: {id: 'Bravo'}},
+//            { data: {id: 'Charlie'}},
+//            { data: {id: 'Delta'}},
+//            { data: {id: 'Echo'}},
+//
+//            { data: {id: 'ab', source: 'Alpha', target: 'Bravo'}},
+//            { data: {id: 'bc', source: 'Bravo', target: 'Charlie'}},
+//            { data: {id: 'ac', source: 'Alpha', target: 'Charlie'}},
+//            { data: {id: 'cd', source: 'Charlie', target: 'Delta'}},
+//            { data: {id: 'da', source: 'Delta', target: 'Alpha'}},
+//            { data: {id: 'db', source: 'Delta', target: 'Bravo'}},
+//
+//            // { data: {id: 'ae', source: 'Alpha', target: 'Echo'}},
+//            // { data: {id: 'be', source: 'Bravo', target: 'Echo'}},
+//            { data: {id: 'ce', source: 'Charlie', target: 'Echo'}},
+//            { data: {id: 'de', source: 'Delta', target: 'Echo'}},
+//      ];
+      for (let sys of aCluster.systems)
+      {
+         retval.push( {group: 'nodes', data: {id: sys.id}});
+      }
+      for (let slipstream of aCluster.slipstreams)
+      {
+         retval.push( {group: 'edges', data: {id: `${slipstream.from.id}${slipstream.to.id}`,
+                                              source: slipstream.from.id,
+                                              target: slipstream.to.id }});
+                                              
+      }
+      return retval;
+   }
 }
