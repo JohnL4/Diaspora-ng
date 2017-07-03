7944295706331eed799c55ff689e5becb33b9689
John Lusk <john-github@how-hard-can-it-be.com>
2017-07-03 18:03:22 -0400

    Email login: html and backing data now set up. Just need to add actual login logic from firebase.

--- src/app/session-ops/session-ops.component.ts
+++ src/app/session-ops/session-ops.component.ts
@@ -17,14 +17,20 @@ export class SessionOpsComponent implements OnInit
    public get user(): Observable<User> { return this._persistenceSvc.currentUser; }
    public uuid: string;
       
-   public get clusterName(): string { return this.cluster.name};
+   public get clusterName(): string { return this.cluster.name; };
    public set clusterName( aName: string)
    {
       // let me = this.constructor.name + '.set clusterName(): ';
-      this.cluster.name = aName
+      this.cluster.name = aName;
       // let jsonStringifiedName = JSON.stringify( aName);
       // console.log( me + `JSON name: ${jsonStringifiedName}`);
    };
+
+   public loggingInWithEmail = false;
+   public isNewEmailAccount = false;
+   public userEmail: string;
+   public emailPassword: string;
+   public emailPassword2: string;
    
    // private getUser(): void
    // {
@@ -39,7 +45,7 @@ export class SessionOpsComponent implements OnInit
    //       });
    // }
 
-   public delayedObservableInfoShowing: boolean = false;
+   public delayedObservableInfoShowing = false;
 
    private get cluster(): Cluster { return this._persistenceSvc.currentClusterSubject.value; }
    
@@ -51,7 +57,7 @@ export class SessionOpsComponent implements OnInit
 
    ngOnInit()
    {
-      const me = this.constructor.name + ".ngOnInit(): ";
+      const me = this.constructor.name + '.ngOnInit(): ';
       console.log( me);
       // this.getUser();           // I think this basically hooks up the promise resolution event.
       this.uuid = UUIDv4.generateUUID();
@@ -59,15 +65,23 @@ export class SessionOpsComponent implements OnInit
 
    public loginWithGoogle()
    {
-      let me = this.constructor.name + ".login(): ";
-      console.log( me + "logging in");
+      const me = this.constructor.name + '.login(): ';
+      console.log( me + 'logging in');
       this._persistenceSvc.login();
       // alert( me + 'done');
    }
 
-   public loginWithEmail()
+   public startLoginWithEmail(): void
    {
+      this.loggingInWithEmail = true;
+   }
 
+   public loginWithEmail(): void
+   {
+      const me = this.constructor.name + 'loginWithEmail(): ';
+      console.log( me 
+         + `Logging in w/email acct ${this.userEmail}, password ${this.emailPassword} ${this.isNewEmailAccount ? '(new account)' : ''}`);
+      this.loggingInWithEmail = false;
    }
 
    public createNewUserWithEmail()
@@ -77,8 +91,8 @@ export class SessionOpsComponent implements OnInit
 
    public logout()
    {
-      let me = this.constructor.name + ".logout(): ";
-      console.log( me + "logging out");
+      const me = this.constructor.name + '.logout(): ';
+      console.log( me + 'logging out');
       // this.user = null;         // Not waiting for an authChanged event for two reasons: (1) the promise has certainly
       //                           // already been resolved, and promises are one-time-only events, and (2) we already know
       //                           // what the outcome of this call will be.
